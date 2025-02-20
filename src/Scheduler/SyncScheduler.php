e6c2bb748ec70f756f7a1542dd28c15fe9eba880
Bozhidar Hristov <warxcell@gmail.com>
2025-02-20 17:27:36 +0200

    FIX

--- src/Scheduler/SyncScheduler.php
+++ src/Scheduler/SyncScheduler.php
@@ -3,11 +3,12 @@ declare(strict_types=1);
 
 namespace Overblog\DataLoaderBundle\Scheduler;
 
+use Closure;
 use GraphQL\Executor\Promise\Adapter\SyncPromise;
 
 final readonly class SyncScheduler implements Scheduler
 {
-    public function scheduleDispatch(\Closure $callback): void
+    public function scheduleDispatch(Closure $callback): void
     {
         SyncPromise::getQueue()->enqueue($callback);
     }
